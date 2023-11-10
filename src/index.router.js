09f1aa67fa3196231a6cf5d66e0c8dac544da3be
abdo zaki <aa1066649@gmail.com>
2023-11-10 03:07:43 +0200

    update index.router.js

--- src/index.router.js
+++ src/index.router.js
@@ -1,9 +1,13 @@
+import morgan from "morgan";
 import connectDB from "../DB/connection.js";
 import authRouter from "./modules/auth/auth.router.js";
 import userRouter from "./modules/user/user.router.js";
 import { globalErrorHandling } from "./utils/errorHandling.js";
 import cors from "cors";
 const bootstrap = (app, express) => {
+  if (process.env.Node_ENV === "dev") {
+    app.use(morgan("dev"));
+  }
   // This is CORS-enabled for all origins
   app.use(cors());
   app.use(express.json());
