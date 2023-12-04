ec1c10e81808da3dcd151a8a66e5fd7b0336dd3c
abdo zaki <aa1066649@gmail.com>
2023-12-04 02:04:44 +0200

    update usermodule

--- src/modules/user/controller/user.js
+++ src/modules/user/controller/user.js
@@ -2,7 +2,11 @@ import userModel from "../../../../DB/models/user.model.js";
 import { asyncHandler } from "../../../utils/errorHandling.js";
 
 export const users = asyncHandler(async (req, res, next) => {
-  const user = await userModel.find({});
+  const user = await userModel
+    .find({})
+    .select(
+      "firstName lastName email phone password gender personalIdCard dateOfBirth"
+    );
   return res.json({ success: true, result: user });
 });
 export const deleteUser = asyncHandler(async (req, res, next) => {
