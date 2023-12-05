8b4fe189ee8d4107ba4aa18c60428d02a0fe84e5
abdo zaki <aa1066649@gmail.com>
2023-12-06 01:58:02 +0200

    add finder model

---
+++ DB/models/finder.model.js
@@ -0,0 +1,49 @@
+import { Schema, Types, model } from "mongoose";
+const FinderSchema = new Schema({
+  userId: {
+    type: Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  finderImage: {
+    secure_url: { type: String, required: true },
+    public_id: { type: String, required: true }
+  },
+  relationMissingPerson: {
+    type: String,
+    enum: [
+      "first degree relative",
+      "second degree relative",
+      "third degree relative",
+      "others"
+    ],
+    required: true
+  },
+  FullName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 50
+  },
+  finderGender: {
+    type: String,
+    enum: ["Male", "Female"],
+    required: true
+  },
+  dateOfbrithFinder: {
+    type: Date,
+    required: true
+  },
+  PhoneResponsibleFinder: {
+    type: String,
+    required: true
+  },
+  DateOfLoss: {
+    type: Date,
+    default: Date.now
+  },
+  HealthStatusFinder: String,
+  birthMark: String,
+  DescriptionMissingPerson: String
+});
+export const finderModel = model("FINDER", FinderSchema);
