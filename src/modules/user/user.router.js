a0bbbe9afd26129f50b973b5b8cf8652a3524d33
abdo zaki <aa1066649@gmail.com>
2023-12-07 01:10:42 +0200

    update usermodel

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 import *as userController from "./controller/user.js";
 const router = Router();
 router.get("/",userController.users);
-router.get("/:_id",userController.deleteUser)
+router.delete("/:_id",userController.deleteUser)
 export default router;
