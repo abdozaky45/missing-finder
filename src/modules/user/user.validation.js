017d8883d9fa32c9a1681688db8f0ec1963289d5
abdo zaki <aa1066649@gmail.com>
2024-02-01 03:10:23 +0200

    update user.validation.js

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -1,6 +1,12 @@
-import Joi from "joi";
-
-export const signup = Joi.object({
-
-    
-})
+import joi from "joi";
+import { Types } from "mongoose";
+const isValidObjectId = (value, helper) => {
+  return Types.ObjectId.isValid(value)
+    ? true
+    : helper.message("In-valid ObjectId");
+};
+export const deleteUser = joi
+  .object({
+    _id: joi.string().custom(isValidObjectId).required()
+  })
+  .required();
