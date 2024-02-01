8cab299fa748bad1c2c969fe63e78624f3820450
abdo zaki <aa1066649@gmail.com>
2024-02-01 03:16:46 +0200

    update userRouter

--- src/modules/user/controller/user.js
+++ src/modules/user/controller/user.js
@@ -10,8 +10,6 @@ export const users = asyncHandler(async (req, res, next) => {
   return res.json({ success: true, result: user });
 });
 export const deleteUser = asyncHandler(async (req, res, next) => {
-  const user = await userModel.findById(req.params._id);
-  if (!user) return next(new Error("In-valid userId!", { cause: 400 }));
   await userModel.findByIdAndDelete(req.params._id);
   return res.json({ success: true, Message: "deleted user" });
 });
