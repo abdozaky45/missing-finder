a4018cd4071f9a3278026d3604586439f68a599b
abdo zaki <aa1066649@gmail.com>
2024-02-01 03:40:18 +0200

    update usermodel

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import * as userController from "./controller/user.js";
-import { validation } from "../../middleware/validation.middelware.js";
-import { deleteUser } from "./user.validation.js";
+
 const router = Router();
 router.get("/", userController.users);
-router.delete("/:_id", validation(deleteUser), userController.deleteUser);
+router.delete("/:_id", userController.deleteUser);
 export default router;
