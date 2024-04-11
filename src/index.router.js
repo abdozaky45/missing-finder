50ded31af73940a9e76e495ae91998af0cc16cc4
Abdulrahman Mohamed Zaki <102440301+abdozaky45@users.noreply.github.com>
2024-04-11 03:04:48 +0200

    Some changes

--- src/index.router.js
+++ src/index.router.js
@@ -2,24 +2,26 @@ import morgan from 'morgan';
 import connectDB from '../DB/connection.js';
 import authRouter from './modules/auth/auth.router.js';
 import userRouter from './modules/user/user.router.js';
-import {globalErrorHandling} from './utils/errorHandling.js';
+import { globalErrorHandling } from './utils/errorHandling.js';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import path from 'path';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import report_missing_personsRouter
   from './modules/report_missing_persons/report_missing_persons.router.js';
 const bootstrap = (app, express) => {
   if (process.env.Node_ENV === 'dev') {
-    app.use (morgan ('dev'));
+    app.use(morgan('dev'));
   }
   // This is CORS-enabled for all origins
-  app.use (cors ());
-  app.use (express.json ());
+  app.use(cors());
+  app.use(express.json());
+  // convert application/x-www-form-urlencoded to json
+  app.use(express.urlencoded({ extended: true }));
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-  const tempDir = path.join (
+  const tempDir = path.join(
     __dirname,
     'src',
     'modules',
@@ -27,21 +29,21 @@ const bootstrap = (app, express) => {
     'controller',
     'custom_temp_folder'
   );
-  app.use (
-    fileUpload ({
+  app.use(
+    fileUpload({
       useTempFiles: true,
       tempFileDir: tempDir,
     })
   );
-  app.use ('/missingfinder', express.static ('missingfinder'));
-  app.use ('/auth', authRouter);
-  app.use ('/user', userRouter);
-  app.use ('/missingPersons', report_missing_personsRouter);
-  app.all ('*', (req, res, next) => {
-    return next (new Error ('page not found!', {cause: 404}));
+  app.use('/missingfinder', express.static('missingfinder'));
+  app.use('/auth', authRouter);
+  app.use('/user', userRouter);
+  app.use('/missingPersons', report_missing_personsRouter);
+  app.all('*', (req, res, next) => {
+    return next(new Error('page not found!', { cause: 404 }));
   });
-  connectDB ();
+  connectDB();
   // ErrorHandling Middleware
-  app.use (globalErrorHandling);
+  app.use(globalErrorHandling);
 };
 export default bootstrap;
